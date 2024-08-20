7e34437cf7164cf49ec7d3cf0b8d1e2a40559a11
Hilshat76 <hilshat76@gmail.com>
2024-08-20 22:48:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-# Для начала определим настройки запуска
-hostName = "localhost" # Адрес для доступа по сети
-serverPort = 8080 # Порт для доступа по сети
+hostName = "localhost"
+serverPort = 8080
 
 class MyServer(BaseHTTPRequestHandler):
     """
@@ -17,14 +16,13 @@ class MyServer(BaseHTTPRequestHandler):
 
     def do_GET(self):
         """ Метод для обработки входящих GET-запросов """
-        self.send_response(200) # Отправка кода ответа
-        self.send_header("Content-type", "text/html") # Отправка типа данных, который будет передаваться
-        self.end_headers() # Завершение формирования заголовков ответа
-        self.wfile.write(bytes(self.get_context_data(), "utf-8")) # Тело ответа
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes(self.get_context_data(), "utf-8"))
 
 if __name__ == "__main__":
-    # Инициализация веб-сервера, который будет по заданным параметрам в сети
-    # принимать запросы и отправлять их на обработку специальному классу, который был описан выше
+
     webServer = HTTPServer((hostName, serverPort), MyServer)
     print("Server started http://%s:%s" % (hostName, serverPort))
 
